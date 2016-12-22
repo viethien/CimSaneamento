ffd4a7033f715347ec91518b9335bc4199e7038d
Tiago Silveira <tiago_tmax@hotmail.com>
2016-12-22 16:21:42 -0200

    Update quemsomos.php

--- CimSaneamento/quemsomos.php
+++ CimSaneamento/quemsomos.php
@@ -1,1 +1,54 @@
+<!doctype HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<!-- Compatibilidade com IE E EDGE-->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<!--Para os Robôs!-->
+<meta name="description" content="Cim Saneamento">
+<meta name="author" content="Tiago Silveira">
+<!--Define a viewport!-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--Inclui o Framework bootstrap/CSS Personalizado/Fontes google!-->
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel= "stylesheet" type="text/css" href="css/estilo.css">
+</head>
+<body>
+<!--Banner (Fora do container geral para pegar a tela toda)!-->
+  <?php include 'includes/nav.html';?><!--Incluí a Navbar!-->
 
+
+<!--========Início conteúdo!=========--->
+<div class="container"><!--Inicio do container geral!-->
+  <div class="row">
+    <div class="col-md-4">
+           <div class="page-header"><h4>Quem somos:</h4></div>
+      <p class="text-left"> A Empresa <strong>CIM Saneamento</strong> existente a mais de quatro décadas é atuante dentro da área de 
+      Saneamento nas atividades de Limpeza e Desobstrução de redes em sistemas de esgoto, águas pluviais e Vídeo Inspeção Robotizada (CFTV).
+      Na área de Trânsito instalamos Equipamentos Classificadores automatizados coletores de dados estatísticos de trânsito rodoviário em todo o território nacional, Balanças rodoviárias para Pesagem 
+      em Movimento de Veículos(WIM), bem como fazemos também a operação viária com serviços de rebocagem e conserva de rodovias.
+      Ainda na área de Trânsito desenvolvemos o projeto, instalação e manutenção de sistemas de iluminação em estradas.
+      Estamos localizados na cidade do Rio de Janeiro e buscamos sempre estar utilizando as mais recentes tecnologias existentes
+      em equipamentos de Vácuo, Sewer Jet, Câmeras de Vídeo e Estatística Rodoviária Automatizada</p>
+      
+           
+    </div><br>
+    <div class="col-md-3">
+      <img src="img/quemsomos/empresa.png" alt="Cim Saneamento">
+      <br>
+    </div>
+  </div>
+<!--========Fim conteúdo!=========--->
+</div> <!--Fim do container Geral!-->
+
+<!--Incluí Footer!-->
+<?php include 'includes/footer.html';?>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- No fim para carregar mais rapido -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</body>
+</html>
