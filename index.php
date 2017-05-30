d1fc4d64c4f55e6ccacca5ed4d890a108673d543
Tiago <tiago_tmax@hotmail.com>
2017-05-29 23:10:21 -0300

    Adicionando arquivos

---
+++ index.php
@@ -0,0 +1,68 @@
+  <?php include 'includes/header.html'; ?>
+  <body>
+    <?php include 'includes/nav.html'; ?>
+   <section class="container-fluid" id="banner">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <h1 class="titulo">Cim Saneamento</h1>
+        <h2 class="subtitulo">Soluções em Inteligentes em Trânsito e Saneamento</h2>
+      </div>
+    </div>
+   </section>
+
+   <section class="container-fluid" id="sobre">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <h1 class="titulo">Quem Somos?</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3 text-center">
+        <h2 class="subtitulo"> Empresa líder no seguimento de execução de serviços públicos e privados nas áreas de Saneamento e Operações Rodoviárias. Possuímos uma linha de equipamentos dentro da mais recente tecnologia disponível para atender nossos clientes</h2>
+        <a href="quemsomos.php"><button type="button" class="botaoprimario" name="button">Ver Mais</button></a>
+      </div>
+    </div>
+
+   </section>
+   <section class="container-fluid" id="atividades">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3 col-sm-12 text-center">
+        <h1 class="titulo">O que nós fazemos?</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3 text-center">
+        <article class="subtitulo">Ofertamos soluções apropriadas de engenharia aos nossos clientes com qualidade diferenciada, valor justo e satisfação.</article>
+        <br />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4 col-md-offset-2 ">
+        <div class="thumbnail text-center">
+         <h1><span class="glyphicon glyphicon-filter titulo2"></span></h1>
+         <div class="caption">
+          <h2>Soluções em Saneamento</h2>
+          <p>
+            Serviços de desobstrução e vídeoinspeção.
+          </p>
+          <a href="saneamento.php"><button type="button" class="botaosecundario" name="button">Ver Mais</button></a>
+         </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="thumbnail text-center">
+         <h1><span class="glyphicon glyphicon-road titulo2"></span></h1>
+         <div class="caption">
+          <h2>Soluções para Rodovias</h2>
+          <p>
+            Serviços de estatística de trânsito e operação de rodovias
+          <a href="transito.php"><button type="button" class="botaosecundario" name="button">Ver Mais</button></a>
+         </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+   <?php include 'includes/footer.html'; ?>
+  </body>
+</html>
---
+++ quemsomos.php
@@ -0,0 +1,75 @@
+<?php include 'includes/header.html'; ?>
+<body>
+  <?php include 'includes/nav.html'; ?>
+ <section class="container-fluid" id="sobrenos">
+  <div class="row">
+    <div class="col-md-12 text-center">
+      <h1 class="titulo">Quem somos?</h1>
+      <h2 class="subtitulo">Aqui você poerá conecer um pouquino a nossa istória.</h2>
+    </div>
+  </div>
+
+   <div class="row">
+       <div class="col-md-6 col-sm-12">
+         <figure>
+           <img src="img/empresa.jpeg">
+         </figure>
+       </div>
+       <article class="col-md-6 col-sm-12">
+           <h1 class="titulo3">Sobre&nbsp;Nós.</h1>
+          <p class="text-left">A Empresa CIM Saneamento existente a mais de quatro décadas é atuante dentro da área de Saneamento nas atividades de Limpeza e Desobstrução de redes em sistemas de esgoto, águas pluviais e Vídeo Inspeção Robotizada (CFTV).
+Na área de Trânsito instalamos Equipamentos Classificadores automatizados coletores de dados estatísticos de trânsito rodoviário em todo o território nacional, Balanças rodoviárias para Pesagem em Movimento de Veículos(WIM), bem como fazemos também a operação viária com serviços de rebocagem e conserva de rodovias.
+Ainda na área de Trânsito desenvolvemos o projeto, instalação e manutenção de sistemas de iluminação em estradas.
+Estamos localizados na cidade do Rio de Janeiro e buscamos sempre estar utilizando as mais recentes tecnologias existentes em equipamentos de Vácuo, Sewer Jet, Câmeras de Vídeo e Estatística Rodoviária Automatizada.
+</p>
+       </article>
+   </div>
+ </section>
+ <section class="container-fluid" id="missao">
+   <div class="row">
+     <div class="col-md-12 text-center">
+       <h1 class="titulo">Missão, Visão e Valores</h1>
+       <h2 class="subtitulo">Principios que regem a nossa empresa.</h2>
+     </div>
+   </div>
+   <div class="row text-center">
+     <div class="col-md-4 text-center">
+       <h2><span class="glyphicon glyphicon-list-alt"></span> Missão</h2>
+       <p>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+nisi ut aliquip ex ea commodo consequat.
+       </p>
+     </div>
+     <div class="col-md-4 text-center">
+       <h2><span class="glyphicon glyphicon-user"></span> Visão</h2>
+       <p>
+         Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+         nisi ut aliquip ex ea commodo consequat.
+              </p>
+     </div>
+     <div class="col-md-4 text-center">
+       <h2><span style="top: 5px;" class="glyphicon glyphicon-briefcase"></span> Valores</h2>
+       <ul>
+         <li>
+           Lorem impsum impsum;
+         </li>
+         <li>
+           Lorem impsum impsum;
+         </li>
+         <li>
+           Lorem impsum impsum;
+         </li>
+         <li>
+           Lorem impsum impsum;
+         </li>
+       </ul>
+     </div>
+   </div>
+ </section>
+ <?php include 'includes/footer.html'; ?>
+</body>
+</html>
